d0dd9d0609ca740b5a094eda0a7b067ede9e9a84
kkkmj <wjsdus531@naver.com>
2023-06-20 23:28:25 +0900

    FIX: code -> statusCode

--- src/main/java/com/foodfinder/oseek/common/CommController.java
+++ src/main/java/com/foodfinder/oseek/common/CommController.java
@@ -8,31 +8,31 @@ public class CommController  {
 
     public ResponseEntity SuccessReturn(Object data) {
         return ResponseEntity.ok().body(
-                ResponseDto.builder().code(ApiCode.SUCCESS.getCode()).codeMsg(ApiCode.SUCCESS.getMsg()).data(data).build());
+                ResponseDto.builder().statusCode(ApiCode.SUCCESS.getCode()).codeMsg(ApiCode.SUCCESS.getMsg()).data(data).build());
     }
 
     public ResponseEntity SuccessReturn() {
         return ResponseEntity.ok().body(
-                ResponseDto.builder().code(ApiCode.SUCCESS.getCode()).codeMsg(ApiCode.SUCCESS.getMsg()).build());
+                ResponseDto.builder().statusCode(ApiCode.SUCCESS.getCode()).codeMsg(ApiCode.SUCCESS.getMsg()).build());
     }
 
     public ResponseEntity ErrorReturn(ApiCode apiCode) {
         return ResponseEntity.ok().body(
-                ResponseDto.builder().code(apiCode.getCode()).codeMsg(apiCode.getMsg()).build());
+                ResponseDto.builder().statusCode(apiCode.getCode()).codeMsg(apiCode.getMsg()).build());
     }
 
     public ResponseEntity ErrorReturn(ApiCode apiCode, Object data) {
         return ResponseEntity.ok().body(
-                ResponseDto.builder().code(apiCode.getCode()).codeMsg(apiCode.getMsg()).data(data).build());
+                ResponseDto.builder().statusCode(apiCode.getCode()).codeMsg(apiCode.getMsg()).data(data).build());
     }
 
     public ResponseEntity ErrorReturn(int code, String msg) {
         return ResponseEntity.ok().body(
-                ResponseDto.builder().code(code).codeMsg(msg).build());
+                ResponseDto.builder().statusCode(code).codeMsg(msg).build());
     }
 
     public ResponseEntity ErrorReturn(int code, String msg, Object data) {
         return ResponseEntity.ok().body(
-                ResponseDto.builder().code(code).codeMsg(msg).data(data).build());
+                ResponseDto.builder().statusCode(code).codeMsg(msg).data(data).build());
     }
 }
